9799e6fa79b3df8472064080e65081349234a332
syariif <syarif15ti@mahasiswa.pcr.ac.id>
2017-10-26 20:03:25 -0700

    punya syarif

---
+++ github/GitHubPertama/CobaGithub.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class CobaGithub
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "Selamat Datang di github"
+    End Sub
+
+End Class
